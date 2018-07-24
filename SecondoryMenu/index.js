89a5721d4dc47273ec735002d71e1c702ffe4569
Gnimgnohcey <40735944+Gnimgnohcey@users.noreply.github.com>
2018-07-24 15:33:00 +0800

    Delete index.js

--- SecondoryMenu/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from  'vue'
-import SecondaryMenu from './SecondaryMenu'
-
-const SMenu={
-  install:function(){
-    Vue.component('SecondaryMenu',SecondaryMenu)
-  }
-}
-export default  SMenu;
